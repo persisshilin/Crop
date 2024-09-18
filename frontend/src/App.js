7f5e1c340dee5ccad9a8d88e055f25d492cf9097
persisshilin <persisshilin974@gmail.com>
2024-09-19 00:54:34 +0530

    Resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ function Form() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    axios.post('https://crop-4.onrender.com/predict ', formData, {
+    axios.post('https://crop-5.onrender.com/predict ', formData, {
       headers: {
         'Content-Type': 'application/json'
       }
